934abea3c70acd77484e7ffda620d3236eed77b1
Sanjay Majumdar <sanjayneal@gmail.com>
2026-06-17 10:34:26 +0545

    Force Hugo extended 0.163.2 via build script

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+set -e
 HUGO_VERSION="0.163.2"
-wget -O hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
-tar -xzf hugo.tar.gz
+echo "Downloading Hugo Extended ${HUGO_VERSION}..."
+wget -q "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz" -O hugo.tar.gz
+tar -xzf hugo.tar.gz hugo
+echo "Hugo version:"
+./hugo version
+echo "Building site..."
 ./hugo
